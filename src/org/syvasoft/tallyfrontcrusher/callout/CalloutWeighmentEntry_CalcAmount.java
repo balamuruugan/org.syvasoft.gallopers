ac8cb768844817bd69249f7d6607c56981935f7a
Syed <syed@syvasoft.com>
2021-09-17 17:39:07 +0530

    Update CalloutWeighmentEntry_CalcAmount.java

--- src/org/syvasoft/tallyfrontcrusher/callout/CalloutWeighmentEntry_CalcAmount.java
+++ src/org/syvasoft/tallyfrontcrusher/callout/CalloutWeighmentEntry_CalcAmount.java
@@ -15,22 +15,14 @@ public class CalloutWeighmentEntry_CalcAmount implements IColumnCallout {
 
 	@Override
 	public String start(Properties ctx, int WindowNo, GridTab mTab, GridField mField, Object value, Object oldValue) {
-		BigDecimal qty = (BigDecimal) mTab.getValue(MWeighmentEntry.COLUMNNAME_NetWeightUnit);
-		BigDecimal price = (BigDecimal) mTab.getValue(MWeighmentEntry.COLUMNNAME_Price);
-		BigDecimal passqty = (BigDecimal) mTab.getValue(MWeighmentEntry.COLUMNNAME_PassQtyIssued);
-		BigDecimal passprice = (BigDecimal) mTab.getValue(MWeighmentEntry.COLUMNNAME_PassPricePerUnit);
-		BigDecimal freightprice  = (BigDecimal) mTab.getValue(MWeighmentEntry.COLUMNNAME_PassPricePerUnit);
+		BigDecimal qty = CalloutUtil.getBDValue(mTab, MWeighmentEntry.COLUMNNAME_NetWeightUnit);
+		BigDecimal price = CalloutUtil.getBDValue(mTab, MWeighmentEntry.COLUMNNAME_Price);
+		BigDecimal passqty = CalloutUtil.getBDValue(mTab, MWeighmentEntry.COLUMNNAME_PassQtyIssued);
+		BigDecimal passprice = CalloutUtil.getBDValue(mTab, MWeighmentEntry.COLUMNNAME_PassPricePerUnit);
+		BigDecimal freightprice  = CalloutUtil.getBDValue(mTab, MWeighmentEntry.COLUMNNAME_FreightPrice);
 		
-		int passID = 0;
-		int freight_uom_id = 0; 
-		
-		if(mTab.getValue(MWeighmentEntry.COLUMNNAME_FreightRule_ID) != null) {
-			freight_uom_id = (int) mTab.getValue(MWeighmentEntry.COLUMNNAME_FreightRule_ID);
-		}
-		
-		if(mTab.getValue(MWeighmentEntry.COLUMNNAME_M_Product_Pass_ID) != null) {
-			passID  = (int) mTab.getValue(MWeighmentEntry.COLUMNNAME_M_Product_Pass_ID);
-		}
+		int passID = CalloutUtil.getIntValue(mTab, MWeighmentEntry.COLUMNNAME_M_Product_Pass_ID);;
+		int freight_uom_id = CalloutUtil.getIntValue(mTab, MWeighmentEntry.COLUMNNAME_FreightUOM_ID);
 		
 		BigDecimal Amount = qty.multiply(price);
 		BigDecimal PassAmount = BigDecimal.ZERO;
@@ -47,16 +39,16 @@ public class CalloutWeighmentEntry_CalcAmount implements IColumnCallout {
 		}
 		
 		if(freight_uom_id > 0) {
-			int KM_UOM_ID = MSysConfig.getIntValue("KM_UOM", 1000071, Env.getAD_Client_ID(ctx));
-			int MT_KM_UOM_ID = MSysConfig.getIntValue("MT_KM_UOM", 1000093, Env.getAD_Client_ID(ctx));
+			//int KM_UOM_ID = MSysConfig.getIntValue("KM_UOM", 1000071, Env.getAD_Client_ID(ctx));
+			//int MT_KM_UOM_ID = MSysConfig.getIntValue("MT_KM_UOM", 1000093, Env.getAD_Client_ID(ctx));
 			int MT_UOM_ID = MSysConfig.getIntValue("MT_KM_UOM", 1000069, Env.getAD_Client_ID(ctx));
 			int LOAD_UOM_ID = MSysConfig.getIntValue("MT_KM_UOM", 1000072, Env.getAD_Client_ID(ctx));
 			
 			if(freight_uom_id == LOAD_UOM_ID) {
-				RentAmount = (BigDecimal) freightprice;
+				RentAmount = freightprice;
 			}	
 			else if(freight_uom_id == MT_UOM_ID) {
-				RentAmount = (BigDecimal) freightprice.multiply(qty);
+				RentAmount = freightprice.multiply(qty);
 			}
 		}
 		
@@ -64,36 +56,32 @@ public class CalloutWeighmentEntry_CalcAmount implements IColumnCallout {
 		
 		BigDecimal TCSAmt = BigDecimal.ZERO;
 		
-		BigDecimal driverTips = BigDecimal.ZERO;		
-		if(driverTips != null) {
-			driverTips	= (BigDecimal) mTab.getValue(MWeighmentEntry.COLUMNNAME_DriverTips);
-		}
+		BigDecimal driverTips = CalloutUtil.getBDValue(mTab, MWeighmentEntry.COLUMNNAME_DriverTips);
 		
-		BigDecimal discountAmt = BigDecimal.ZERO;
-		if(discountAmt != null) {
-			discountAmt = (BigDecimal) mTab.getValue(MWeighmentEntry.COLUMNNAME_DiscountAmount);
-		}
+		
+		BigDecimal discountAmt = CalloutUtil.getBDValue(mTab,MWeighmentEntry.COLUMNNAME_DiscountAmount);
 		
 		Boolean ApplyTax = mTab.getValueAsBoolean(MWeighmentEntry.COLUMNNAME_IsPermitSales);
 		Boolean ApplyTCS = mTab.getValueAsBoolean(MWeighmentEntry.COLUMNNAME_ApplyTCS);
 		//if(ApplyTax)
-		GstAmt = PassAmount.add(Amount).multiply(new BigDecimal(0.05)).setScale(2, RoundingMode.HALF_EVEN);
+		GstAmt = RentAmount.add(Amount).multiply(new BigDecimal(0.05)).setScale(2, RoundingMode.HALF_EVEN);
 		/*else
 			GstAmt = BigDecimal.ZERO;
 		*/
 		BigDecimal TotalAmount = Amount.add(GstAmt).add(RentAmount).add(PassAmount);
 		
-		if(ApplyTCS) {
+		if(ApplyTCS && ApplyTax) {
 			TCSAmt = TotalAmount.multiply(new BigDecimal(0.001)).setScale(2, RoundingMode.HALF_EVEN);
 		}
 			
 		GrandTotalAmt = TotalAmount.add(TCSAmt).subtract(driverTips).subtract(discountAmt);
-		
+		mTab.setValue(MWeighmentEntry.COLUMNNAME_GrossPrice, price);		
 		mTab.setValue(MWeighmentEntry.COLUMNNAME_Rent_Amt, RentAmount);
 		mTab.setValue(MWeighmentEntry.COLUMNNAME_GSTAmount, GstAmt);		
 		mTab.setValue(MWeighmentEntry.COLUMNNAME_Amount, Amount);
 		mTab.setValue(MWeighmentEntry.COLUMNNAME_PermitPassAmount, PassAmount);
 		mTab.setValue(MWeighmentEntry.COLUMNNAME_TotalAmt, GrandTotalAmt);
+		mTab.setValue(MWeighmentEntry.COLUMNNAME_TCSAmount, TCSAmt);
 		return null;
 	}
 
