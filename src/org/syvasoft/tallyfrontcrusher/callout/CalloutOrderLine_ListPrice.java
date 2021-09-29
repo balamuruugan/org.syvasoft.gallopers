7ae991cca7b4aa591daafb1206026e9580e1761d
Syed <syed@syvasoft.com>
2021-09-29 17:56:58 +0530

    Discount Issue fixed

--- src/org/syvasoft/tallyfrontcrusher/callout/CalloutOrderLine_ListPrice.java
+++ src/org/syvasoft/tallyfrontcrusher/callout/CalloutOrderLine_ListPrice.java
@@ -12,9 +12,10 @@ public class CalloutOrderLine_ListPrice implements IColumnCallout {
 
 	@Override
 	public String start(Properties ctx, int WindowNo, GridTab mTab, GridField mField, Object value, Object oldValue) {
-		BigDecimal PriceList = CalloutUtil.getBDValue(mTab, TF_MOrderLine.COLUMNNAME_PriceList);
+		BigDecimal PriceList = CalloutUtil.getBDValue(mTab, TF_MOrderLine.COLUMNNAME_PriceList);		
 		mTab.setValue(TF_MOrderLine.COLUMNNAME_Discount, BigDecimal.ZERO);
 		mTab.setValue(TF_MOrderLine.COLUMNNAME_PriceEntered, PriceList);
+		mTab.setValue(TF_MOrderLine.COLUMNNAME_PriceActual, PriceList);
 		
 		return null;
 	}
