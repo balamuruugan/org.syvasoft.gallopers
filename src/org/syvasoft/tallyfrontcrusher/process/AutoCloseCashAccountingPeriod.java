94b18e178f668c9934f464e99a17bcb1b9bccf08
Syed <syed@syvasoft.com>
2021-10-27 11:19:57 +0530

    Update AutoCloseCashAccountingPeriod.java
    Bug Fixed

--- src/org/syvasoft/tallyfrontcrusher/process/AutoCloseCashAccountingPeriod.java
+++ src/org/syvasoft/tallyfrontcrusher/process/AutoCloseCashAccountingPeriod.java
@@ -19,15 +19,18 @@ public class AutoCloseCashAccountingPeriod extends SvrProcess {
 
 	@Override
 	protected String doIt() throws Exception {
+		Timestamp today = new Timestamp(TimeUtil.getToday().getTimeInMillis());
+		Timestamp date = TimeUtil.getPreviousDay(today);
+		
 		String whereClause = "C_BankAccount_ID NOT IN (SELECT C_BankAccount_ID FROM "
-				+ "TF_AcctPeriod WHERE DateTo <= now() - 1)";
+				+ "TF_AcctPeriod WHERE DateTo = Trunc(?::timestamp))";
 		List<TF_MBankAccount> list = new Query(getCtx(), TF_MBankAccount.Table_Name, whereClause, get_TrxName())
 				.setClient_ID()
+				.setParameters(date)
 				.setOnlyActiveRecords(true)
 				.list();
 		
-		Timestamp today = new Timestamp(TimeUtil.getToday().getTimeInMillis());
-		Timestamp date = TimeUtil.getPreviousDay(today);
+		
 		int i = 0;
 		for(TF_MBankAccount b : list) {
 			MCashAcctPeriod p = new MCashAcctPeriod(getCtx(), 0, get_TrxName());
