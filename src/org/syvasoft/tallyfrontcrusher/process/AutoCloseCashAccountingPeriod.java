21dc153fe9993bea98259d8362014bf5634d8455
Syed <syed@syvasoft.com>
2021-10-28 12:19:33 +0530

    #116
    Auto Lock for both BP and Cash acct

--- src/org/syvasoft/tallyfrontcrusher/process/AutoCloseCashAccountingPeriod.java
+++ src/org/syvasoft/tallyfrontcrusher/process/AutoCloseCashAccountingPeriod.java
@@ -6,7 +6,9 @@ import java.util.List;
 import org.compiere.model.Query;
 import org.compiere.process.SvrProcess;
 import org.compiere.util.TimeUtil;
+import org.syvasoft.tallyfrontcrusher.model.MBPAcctPeriod;
 import org.syvasoft.tallyfrontcrusher.model.MCashAcctPeriod;
+import org.syvasoft.tallyfrontcrusher.model.TF_MBPartner;
 import org.syvasoft.tallyfrontcrusher.model.TF_MBankAccount;
 
 public class AutoCloseCashAccountingPeriod extends SvrProcess {
@@ -33,16 +35,65 @@ public class AutoCloseCashAccountingPeriod extends SvrProcess {
 		
 		int i = 0;
 		for(TF_MBankAccount b : list) {
-			MCashAcctPeriod p = new MCashAcctPeriod(getCtx(), 0, get_TrxName());
-			p.setAD_Org_ID(b.getAD_Org_ID());
-			p.setC_BankAccount_ID(b.getC_BankAccount_ID());
-			p.setDateFrom(date);
+			whereClause = "AD_Org_ID = ? AND C_BankAccount_ID = ?";
+			
+			MCashAcctPeriod p = new Query(getCtx(), MCashAcctPeriod.Table_Name, whereClause, get_TrxName())
+					.setClient_ID()
+					.setParameters(b.getAD_Org_ID(), b.getC_BankAccount_ID())
+					.setOrderBy("DateTo DESC")
+					.first();			
+			
+			if(p == null) {
+				p = new MCashAcctPeriod(getCtx(), 0, get_TrxName());
+				p.setAD_Org_ID(b.getAD_Org_ID());
+				p.setC_BankAccount_ID(b.getC_BankAccount_ID());
+				p.setDateFrom(date);				
+			}
+			
 			p.setDateTo(date);
 			p.saveEx();
 			i++;
 		}
 		
+		whereClause = "(IsCustomer='Y' OR IsVendor='Y') AND C_BPartner_ID NOT IN (SELECT C_BPartner_ID FROM "
+				+ "TF_BPAcctPeriod WHERE DateTo = Trunc(?::timestamp))";
+		List<TF_MBPartner> bpList = new Query(getCtx(), TF_MBPartner.Table_Name, whereClause, get_TrxName())
+				.setClient_ID()
+				.setOnlyActiveRecords(true)
+				.setParameters(date)
+				.list();
+		
+		for(TF_MBPartner bp : bpList) {
+			if(bp.getAD_Org_ID() > 0)
+				createBPLockPeriod(bp.getAD_Org_ID(), bp.getC_BPartner_ID(), date);
+			else {
+				createBPLockPeriod(1000003, bp.getC_BPartner_ID(), date);
+				createBPLockPeriod(1000000, bp.getC_BPartner_ID(), date);
+			}
+			i++;
+		}
+		
 		return i + " records are created successfully!";
+	}
+	
+	private void createBPLockPeriod(int AD_Org_ID, int C_BPartner_ID, Timestamp date) {
+		String whereClause = "AD_Org_ID = ? AND C_BPartner_ID = ? ";
+		MBPAcctPeriod p = new Query(getCtx(), MBPAcctPeriod.Table_Name, whereClause, get_TrxName())
+				.setClient_ID()
+				.setParameters(AD_Org_ID, C_BPartner_ID)
+				.setOrderBy("DateTo DESC")
+				.first();
+		if(p == null) {
+			p = new MBPAcctPeriod(getCtx(), 0, get_TrxName());
+			p.setAD_Org_ID(AD_Org_ID);
+			p.setC_BPartner_ID(C_BPartner_ID);
+			p.setDateFrom(date);
+			p.setDateTo(date);	
+		}
+		else
+			p.setDateTo(date);
+		
+		p.saveEx();
 	}
 
 }
