fd0ce5f968e0eb20eb201a23ed6ad0b17216d2ca
Syed <syed@syvasoft.com>
2021-11-11 17:23:00 +0530

    Two days old entry lock

--- src/org/syvasoft/tallyfrontcrusher/process/AutoCloseCashAccountingPeriod.java
+++ src/org/syvasoft/tallyfrontcrusher/process/AutoCloseCashAccountingPeriod.java
@@ -22,7 +22,7 @@ public class AutoCloseCashAccountingPeriod extends SvrProcess {
 	@Override
 	protected String doIt() throws Exception {
 		Timestamp today = new Timestamp(TimeUtil.getToday().getTimeInMillis());
-		Timestamp date = TimeUtil.getPreviousDay(today);
+		Timestamp date = TimeUtil.getPreviousDay(TimeUtil.getPreviousDay(today));
 		
 		String whereClause = "C_BankAccount_ID NOT IN (SELECT C_BankAccount_ID FROM "
 				+ "TF_AcctPeriod WHERE DateTo = Trunc(?::timestamp))";
